0191b57bc98eb1f4485dcec198aa8a3aef214b97
Praveen jha <Praveenjhafbg54321@gmail.com>
2022-11-13 13:18:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,21 +4,39 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='h1'>
+        Currency Converter 
+      </h1>
+
+      <div className='praveen'>
+        <h1 className='top-header'>Convert Your Currency Here</h1>
+       Enter Currency In Indian Rs &nbsp;&nbsp;<input id='CurrencyInput'/>
+
+      <div className='SelectCountry'>
+        Select the Country &nbsp;&nbsp;
+       <select name="city" id="city" required class="special">
+          <option value="delhi">USA</option>
+          <option value="jodhpur">NEPAL</option>
+          <option value="raipur">DUBAI</option>
+          <option value="mumbai">AUSTRALIA</option>
+          <option value="bihar">UK</option>
+          <option value="mop">Others</option>
+        </select>
+
+       </div>
+       <div className='submit'>
+        <button className='convert_button'>Convert</button>
+       </div>
+
+       <div className='result'>
+        200 Rs is = 2.4844 US Dollar
+       </div>
+      </div>
+
+      
     </div>
+
+    
   );
 }
 
